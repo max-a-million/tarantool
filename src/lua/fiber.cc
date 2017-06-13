ea23fcdbed797cb34da7c79dc82eb873f14fb445
GeorgyKirichenko <kirichenkoga@gmail.com>
2017-06-13 17:42:44 +0300

    Use libunwind for backtraces
    Remove libbfd from dependencies and use libunwind for stack traces.
    
    Closes #2103

--- src/lua/fiber.cc
+++ src/lua/fiber.cc
@@ -37,6 +37,7 @@
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
+#include <cxxabi.h>
 
 void
 luaL_testcancel(struct lua_State *L)
@@ -183,11 +184,14 @@ static int
 fiber_backtrace_cb(int frameno, void *frameret, const char *func, size_t offset, void *cb_ctx)
 {
 	char buf[512];
-	int l = snprintf(buf, sizeof(buf), "#%-2d %p in ", frameno, frameret);
-	if (func)
-		snprintf(buf + l, sizeof(buf) - l, "%s+%zu", func, offset);
+	int status, l = snprintf(buf, sizeof(buf), "#%-2d %p in ", frameno, frameret);
+	char *demangled = abi::__cxa_demangle(func, 0, 0, &status);
+	if (demangled || func)
+		snprintf(buf + l, sizeof(buf) - l, "%s+%zu",
+			 demangled ? demangled : func, offset);
 	else
 		snprintf(buf + l, sizeof(buf) - l, "?");
+	free(demangled);
 	struct lua_State *L = (struct lua_State*)cb_ctx;
 	lua_pushnumber(L, frameno + 1);
 	lua_pushstring(L, buf);
@@ -230,11 +234,8 @@ lbox_fiber_statof(struct fiber *f, void *cb_ctx)
 #ifdef ENABLE_BACKTRACE
 	lua_pushstring(L, "backtrace");
 	lua_newtable(L);
-	if (f != fiber()) {
-		backtrace_foreach(fiber_backtrace_cb,
-				  f->last_stack_frame,
-				  f->stack, f->stack_size, L);
-	}
+	if (f != fiber())
+		backtrace_foreach(fiber_backtrace_cb, &f->ctx, L);
 	lua_settable(L, -3);
 #endif /* ENABLE_BACKTRACE */
 
