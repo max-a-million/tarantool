502db4ed7f2cc9f45c592d72a089ff67f4211775
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2017-07-14 21:33:14 +0300

    sql: fix sql_prepare_and_execute broken yield
    Closes #2601

--- test/sql/iproto.test.lua
+++ test/sql/iproto.test.lua
@@ -119,6 +119,23 @@ cn:execute('create index test2_a_b_index on test2(a, b)')
 cn:execute('drop table test2')
 box.space.test2
 
+-- gh-2601 iproto messages are corrupted
+errinj = box.error.injection
+fiber = require('fiber')
+errinj.set("ERRINJ_WAL_DELAY", true)
+insert_res = nil
+select_res = nil
+function execute_yield() insert_res = cn:execute('insert into test values (100, 1, "1")') end
+function execute_notyield() select_res = cn:execute('select 1') end
+f1 = fiber.create(execute_yield)
+while f1:status() ~= 'suspended' do fiber.sleep(0) end
+f2 = fiber.create(execute_notyield)
+while f2:status() ~= 'dead' do fiber.sleep(0) end
+errinj.set("ERRINJ_WAL_DELAY", false)
+while f1:status() ~= 'dead' do fiber.sleep(0) end
+insert_res
+select_res
+
 cn:close()
 box.schema.user.revoke('guest', 'read,write,execute', 'universe')
 box.sql.execute('drop table test')
